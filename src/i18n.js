8eebbcf6ef2890f53117f0854f5ff44272105a48
tfox121 <tfox2@hotmail.co.uk>
2020-06-30 21:14:25 +0100

    Adjust i18n config to address build issues.

--- src/i18n.js
+++ src/i18n.js
@@ -14,33 +14,25 @@ const enTranslationMessages = require('./translations/en.json');
 
 addLocaleData(enLocaleData);
 
-const DEFAULT_LOCALE = 'en';
+export const DEFAULT_LOCALE = 'en';
 
-// prettier-ignore
-const appLocales = [
+export const appLocales = [
   'en',
 ];
 
-const formatTranslationMessages = (locale, messages) => {
-  const defaultFormattedMessages =
-    locale !== DEFAULT_LOCALE
-      ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
-      : {};
+export const formatTranslationMessages = (locale, messages) => {
+  const defaultFormattedMessages = locale !== DEFAULT_LOCALE
+    ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
+    : {};
   const flattenFormattedMessages = (formattedMessages, key) => {
-    const formattedMessage =
-      !messages[key] && locale !== DEFAULT_LOCALE
-        ? defaultFormattedMessages[key]
-        : messages[key];
+    const formattedMessage = !messages[key] && locale !== DEFAULT_LOCALE
+      ? defaultFormattedMessages[key]
+      : messages[key];
     return Object.assign(formattedMessages, { [key]: formattedMessage });
   };
   return Object.keys(messages).reduce(flattenFormattedMessages, {});
 };
 
-const translationMessages = {
+export const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
 };
-
-exports.appLocales = appLocales;
-exports.formatTranslationMessages = formatTranslationMessages;
-exports.translationMessages = translationMessages;
-exports.DEFAULT_LOCALE = DEFAULT_LOCALE;
